252641ffc7d8cf2232b46e4ec49935c9533964fc
zhai <jue.yguel@gmail.com>
2019-07-08 16:59:47 +0200

    deuxieme journée

---
+++ app/app.php
@@ -0,0 +1,1 @@
+Mon appli est démarrée !
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+/**
+ * FIchier de démarrage de l'application
+ */
+
+ // 1. Intégration du fichier d'execution de l'application
+
+ require_once "../app/app.php";
