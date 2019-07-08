42fb5e8bfb4845f5c85e3526a8a5094e0bf456fd
zhai <jue.yguel@gmail.com>
2019-07-08 17:34:51 +0200

    -

--- public/index.php
+++ public/index.php
@@ -8,3 +8,20 @@
  // 1. Intégration du fichier d'execution de l'application
 
  require_once "../app/app.php";
+
+//02. Intégration de la configuration
+
+//03. Définition de l'environnement
+
+//04. Comportement des errreurs
+
+//05. Connexions aux bases de données
+
+//06. Routage de l'application
+
+//07. Inclusion des fonctions Utils
+
+//08. Compilation de la page (part 1)
+
+//09. Compilation de la page (part 2)
+ 
